dd46d35979b7fca6ea46164b25c492a230f7dbe5
BradDesktop <brad.martin@xdisplay.com>
2023-06-21 12:16:46 -0400

    cool tap animation

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -33,10 +33,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  double _tapshadow = 0;
+
   double _x = pi;
   double _y = pi;
-  double _dragx = 0;
-  double _dragy = 0;
+
   Size _size = Size.zero;
   final GlobalKey _key = GlobalKey();
 
@@ -44,28 +45,51 @@ class _MyHomePageState extends State<MyHomePage> {
     final RenderBox renderBox =
         _key.currentContext!.findRenderObject() as RenderBox;
     _size = renderBox.size;
-    _dragx = 0;
-    _dragy = 0;
   }
 
   void _getDragPosition(DragUpdateDetails details) {
     final RenderBox box = _key.currentContext!.findRenderObject() as RenderBox;
     final Offset localOffset = box.globalToLocal(details.globalPosition);
-    _dragx = localOffset.dx;
-    _dragy = localOffset.dy;
+    double dragx = localOffset.dx;
+    double dragy = localOffset.dy;
     setState(() => {
-          _x = _dragy - _size.width / 2,
-          _y = _dragx - _size.height / 2,
+          _x = dragy - _size.width / 2,
+          _y = dragx - _size.height / 2,
+          if (!context.isPhone)
+            {
+              _x = _x / (_size.width / 200),
+              _y = _y / (_size.height / 200),
+            }
         });
+  }
+
+  void _endDrag(DragEndDetails details) {
+    if (context.isPhone) {
+      setState(() => {_x = 0, _y = 0});
+    }
   }
 
   void _afterLayout(_) {
     _getSize();
   }
 
-  void _incrementCounter() {
+  void _performTap() {
     setState(() {
       _counter++;
+      _tapshadow = 50;
+    });
+  }
+
+  void _relieveTap() {
+    // Async loop to reduce shadow to 0 over 0.5 seconds
+    Future.doWhile(() async {
+      await Future.delayed(const Duration(milliseconds: 10));
+      setState(() {
+        if (_tapshadow > 0) {
+          _tapshadow -= 1;
+        }
+      });
+      return _tapshadow > 0;
     });
   }
 
@@ -84,10 +108,12 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: GestureDetector(
+        onTapDown: (_) => _performTap(),
+        onTapUp: (_) => _relieveTap(),
         onVerticalDragUpdate: (details) => _getDragPosition(details),
         onHorizontalDragUpdate: (details) => _getDragPosition(details),
-        onVerticalDragEnd: (details) => setState(() => {_x = 0, _y = 0}),
-        onHorizontalDragEnd: (details) => setState(() => {_x = 0, _y = 0}),
+        onVerticalDragEnd: (details) => _endDrag(details),
+        onHorizontalDragEnd: (details) => _endDrag(details),
         child: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
@@ -101,15 +127,13 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 MouseRegion(
                   key: _key,
-                  cursor: SystemMouseCursors.move,
+                  cursor: SystemMouseCursors.click,
                   onHover: (event) => setState(() => {
                         if (!context.isPhone)
                           {
                             _getSize(),
                             _x = event.localPosition.dy - _size.width / 2,
                             _y = event.localPosition.dx - _size.height / 2,
-                            // Adjust the range of x and y from
-                            //[-(size.x/y * 0.005), (size.x/y * 0.005)] to [-1, 1]
                             _x = _x / (_size.width / 200),
                             _y = _y / (_size.height / 200),
                           }
@@ -119,42 +143,31 @@ class _MyHomePageState extends State<MyHomePage> {
                         _y = 0,
                       }),
                   child: Transform(
-                    transform: Matrix4.inverted(Matrix4.identity()
+                    transform: Matrix4.identity()
                       ..rotateX(_x * -0.01)
-                      ..rotateY(_y * 0.01)),
+                      ..rotateY(_y * 0.01),
                     alignment: FractionalOffset.center,
-                    child: Material(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
+                    child: Container(
+                      height: MediaQuery.of(context).size.shortestSide * 0.5,
+                      width: MediaQuery.of(context).size.shortestSide * 0.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                            MediaQuery.of(context).size.shortestSide * 0.1),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 153, 235, 194),
+                            blurRadius: 20,
+                            offset: Offset(-_y / 2, -_x / 2),
+                            spreadRadius: _tapshadow,
+                          ),
+                        ],
+                        color: const Color.fromARGB(62, 33, 149, 243),
                       ),
-                      elevation: 50,
-                      child: Container(
-                        // Set height and width to be 0.5 the min dimension of the screen
-                        height: MediaQuery.of(context).size.shortestSide * 0.5,
-                        width: MediaQuery.of(context).size.shortestSide * 0.5,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                              color: const Color.fromARGB(255, 153, 235, 194),
-                              blurRadius: 20,
-                              offset: Offset(-_y / 2, -_x / 2),
-                            ),
-                          ],
-                          gradient: RadialGradient(
-                            colors: [
-                              const Color.fromARGB(255, 153, 235, 194),
-                              Theme.of(context).colorScheme.primary,
-                            ],
-                          ),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Center(
-                          child: Text(
-                            '$_counter',
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.headlineLarge,
-                          ),
+                      child: Center(
+                        child: Text(
+                          '$_counter',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.headlineLarge,
                         ),
                       ),
                     ),
@@ -165,11 +178,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
