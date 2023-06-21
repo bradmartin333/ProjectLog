b3180fa1eb2eba7ae016caaf4a7c167f005c50bd
BradDesktop <brad.martin@xdisplay.com>
2023-06-21 11:35:10 -0400

    working mobile and web

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -35,17 +35,32 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   double _x = pi;
   double _y = pi;
+  double _dragx = 0;
+  double _dragy = 0;
   Size _size = Size.zero;
-  GlobalKey _key = GlobalKey();
+  final GlobalKey _key = GlobalKey();
 
-  void _getSizes() {
+  void _getSize() {
     final RenderBox renderBox =
         _key.currentContext!.findRenderObject() as RenderBox;
     _size = renderBox.size;
+    _dragx = 0;
+    _dragy = 0;
+  }
+
+  void _getDragPosition(DragUpdateDetails details) {
+    final RenderBox box = _key.currentContext!.findRenderObject() as RenderBox;
+    final Offset localOffset = box.globalToLocal(details.globalPosition);
+    _dragx = localOffset.dx;
+    _dragy = localOffset.dy;
+    setState(() => {
+          _x = _dragy - _size.width / 2,
+          _y = _dragx - _size.height / 2,
+        });
   }
 
   void _afterLayout(_) {
-    _getSizes();
+    _getSize();
   }
 
   void _incrementCounter() {
@@ -68,61 +83,86 @@ class _MyHomePageState extends State<MyHomePage> {
         foregroundColor: Colors.white,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MouseRegion(
-              key: _key,
-              cursor: SystemMouseCursors.move,
-              onHover: (event) => setState(() => {
-                    _getSizes(),
-                    _x = -0.01 * (event.localPosition.dy - _size.width / 2),
-                    _y = 0.01 * (event.localPosition.dx - _size.height / 2),
-                    // Adjust the range of x and y from
-                    //[-(size.x/y * 0.005), (size.x/y * 0.005)] to [-1, 1]
-                    _x = _x / (_size.width / 200),
-                    _y = _y / (_size.height / 200),
-                  }),
-              onExit: (event) => setState(() => {
-                    _x = 0,
-                    _y = 0,
-                  }),
-              child: Transform(
-                transform: Matrix4.inverted(Matrix4.identity()
-                  ..rotateX(_x)
-                  ..rotateY(_y)),
-                alignment: FractionalOffset.center,
-                child: Material(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  elevation: 100,
-                  child: Container(
-                    height: 300.0,
-                    width: 300.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: RadialGradient(
-                        colors: [
-                          const Color.fromARGB(255, 153, 235, 194),
-                          Theme.of(context).colorScheme.primary,
-                        ],
+      body: GestureDetector(
+        onVerticalDragUpdate: (details) => _getDragPosition(details),
+        onHorizontalDragUpdate: (details) => _getDragPosition(details),
+        onVerticalDragEnd: (details) => setState(() => {_x = 0, _y = 0}),
+        onHorizontalDragEnd: (details) => setState(() => {_x = 0, _y = 0}),
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('lib/images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                MouseRegion(
+                  key: _key,
+                  cursor: SystemMouseCursors.move,
+                  onHover: (event) => setState(() => {
+                        if (!context.isPhone)
+                          {
+                            _getSize(),
+                            _x = event.localPosition.dy - _size.width / 2,
+                            _y = event.localPosition.dx - _size.height / 2,
+                            // Adjust the range of x and y from
+                            //[-(size.x/y * 0.005), (size.x/y * 0.005)] to [-1, 1]
+                            _x = _x / (_size.width / 200),
+                            _y = _y / (_size.height / 200),
+                          }
+                      }),
+                  onExit: (event) => setState(() => {
+                        _x = 0,
+                        _y = 0,
+                      }),
+                  child: Transform(
+                    transform: Matrix4.inverted(Matrix4.identity()
+                      ..rotateX(_x * -0.01)
+                      ..rotateY(_y * 0.01)),
+                    alignment: FractionalOffset.center,
+                    child: Material(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
                       ),
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: Center(
-                      child: Text(
-                        '$_counter',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headlineLarge,
+                      elevation: 50,
+                      child: Container(
+                        // Set height and width to be 0.5 the min dimension of the screen
+                        height: MediaQuery.of(context).size.shortestSide * 0.5,
+                        width: MediaQuery.of(context).size.shortestSide * 0.5,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: const Color.fromARGB(255, 153, 235, 194),
+                              blurRadius: 20,
+                              offset: Offset(-_y / 2, -_x / 2),
+                            ),
+                          ],
+                          gradient: RadialGradient(
+                            colors: [
+                              const Color.fromARGB(255, 153, 235, 194),
+                              Theme.of(context).colorScheme.primary,
+                            ],
+                          ),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: Center(
+                          child: Text(
+                            '$_counter',
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context).textTheme.headlineLarge,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
